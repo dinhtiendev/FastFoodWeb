89300e95a7a48d1e8973a4dc1c10674ab9bc3eb9
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2022-07-01 16:42:26 +0700

    Add project files.

---
+++ FastFoodWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FastFoodWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
