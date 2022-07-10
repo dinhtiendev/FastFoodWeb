f4c644765c10c406b4b9a06e1ebfe8ed420b14c7
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2022-07-10 12:27:37 +0700

    Add Login LoigOut and Register

--- FastFoodWeb/Controllers/LogController.cs
+++ FastFoodWeb/Controllers/LogController.cs
@@ -1,22 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastFoodWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace FastFoodWeb.Controllers
 {
     public class LogController : Controller
     {
-        public IActionResult LogIn()
+        public IActionResult LogIn(string email, string password, bool isSave)
         {
-            return View();
+            using (var context = new FastFoodContext())
+            {
+                Account account = context.Accounts.FirstOrDefault(x => x.Email.Equals(email) && x.Password.Equals(password));
+                if (account != null)
+                {
+                    HttpContext.Session.SetString("Account", JsonConvert.SerializeObject(account));
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult LogOut()
         {
-            return View();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
 
-        public IActionResult Register()
+        public IActionResult Register(string name, string phone, string email, string password)
         {
-            return View();
+            using (var context = new FastFoodContext())
+            {
+                if (context.Accounts.Any(x => !x.Email.Equals(email)))
+                {
+                    Account account = new Account { Name = name, Phone = phone, Email = email, Password = password, IsAdmin = false, IsActive = true };
+                    context.Accounts.Add(account);
+                    context.SaveChanges();
+                    HttpContext.Session.SetString("Account", JsonConvert.SerializeObject(account));
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- FastFoodWeb/Startup.cs
+++ FastFoodWeb/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,17 +13,12 @@ namespace FastFoodWeb
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,13 +28,11 @@ namespace FastFoodWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseSession();
 
             app.UseAuthorization();
 
@@ -46,7 +40,7 @@ namespace FastFoodWeb
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Shop}/{id=0}/{page=1}/{info=}");
+                    pattern: "{controller=Home}/{action=Index}/{id=0}/{page=1}/{info=}");
             });
         }
     }
